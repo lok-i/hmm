3ca0a07b71c682e1d630cd59dea0bf5ea9d190d1
lok-i <lokesha1b2c3@gmail.com>
2022-02-01 00:03:06 +0530

    added marker data, conf, visual tool, intm

---
+++ read_markers.py
@@ -0,0 +1,86 @@
+import c3d
+import numpy as np
+import matplotlib.pyplot as plt
+import yaml
+import matplotlib.pyplot as plt
+from pytransform3d.transformations import   random_transform,transform_from, plot_transform
+from pytransform3d.rotations import random_axis_angle, matrix_from_axis_angle
+from pytransform3d.plot_utils import make_3d_axis, plot_capsule
+
+
+random_state = np.random.RandomState(42)
+# ax = make_3d_axis(1)
+
+assets_path = './gym_hmm_ec/envs/assets/our_data/'
+marker_conf_file_name = 'marker_config.yaml'
+c3d_file_name = 'mocap_data/Trial_1.c3d'
+
+
+config_file = open(assets_path + marker_conf_file_name,'r+')
+marker_conf = yaml.load(config_file, Loader=yaml.FullLoader)
+
+link_marker_info = marker_conf['segment2markers']
+ 
+pts_to_plt = []
+with open( assets_path+c3d_file_name , 'rb') as handle:
+
+    reader = c3d.Reader(handle)
+    for data in reader.read_frames():
+
+        # print('Frame {}'.format(data[0],data[1].shape,data[2][0]))
+        all_marker = []
+        
+        if data[0] % 100 == 0:
+            for pt in data[1]:
+                all_marker.append(pt[0:3].tolist())
+        
+            pts_to_plt.append(all_marker)
+        
+        # if data[0] > 100:
+        #     break
+pts_to_plt = np.array(pts_to_plt)
+
+
+time_step = 0
+marker_pos_at_time_step = pts_to_plt[time_step]
+print(marker_pos_at_time_step.shape)
+
+link_name = 'right_upper_leg'
+
+marker_index_of_link = list(link_marker_info[link_name].keys())
+pts_in_link = []
+for marker_index in marker_index_of_link:
+
+    marker_pos_in_m = 0.001* marker_pos_at_time_step[marker_index,0:3] # mm -> m
+    pts_in_link.append(marker_pos_in_m.tolist())
+
+pts_in_link = np.array(pts_in_link)
+
+# print(pts_to_plt.shape)
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+
+for marker_index, pt in zip( marker_index_of_link ,pts_in_link ):
+    marker_name = link_marker_info[link_name][marker_index]
+    ax.scatter(pt[0],pt[1],pt[2],label=marker_name)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+ax.legend()
+
+capsule2origin = np.eye(4)#random_transform(random_state)
+for i in range(3):
+    capsule2origin[i][3] = np.mean(pts_in_link[:,i])
+
+# print(capsule2origin)
+
+height = 0.2
+radius = 0.05
+plot_transform(ax=ax, A2B=capsule2origin, s=0.03)
+plot_capsule(ax=ax, A2B=capsule2origin, height=height, radius=radius,
+             color="r", alpha=0.5, wireframe=False)
+
+plt.show()
+
--- test_env.py
+++ test_env.py
@@ -1,47 +1,30 @@
 
-from dm_control import suite
-from dm_control import viewer
+from gym_hmm_ec.envs.bipedal_env import BipedEnv 
+from gym_hmm_ec.controllers.pd_controller import PDController 
+import matplotlib.pyplot as plt
 import numpy as np
+from mujoco_py import functions
 
-env = suite.load(domain_name="humanoid", task_name="stand")
-action_spec = env.action_spec()
+# environment config and setup
+env_conf = {
+            'set_on_rack': False,
+            'render': True,
+            'model_name':'humanoid_no_hands',
+            'mocap':False
+            }
 
-# Define a uniform random policy.
-def random_policy(time_step):
-  del time_step  # Unused.
-  return np.random.uniform(low=action_spec.minimum,
-                           high=action_spec.maximum,
-                           size=action_spec.shape)
+env = BipedEnv(**env_conf)
 
-# Launch the viewer application.
-viewer.launch(env, policy=random_policy)
+# initialse the env,reset simualtion
+env.reset()
 
-# from gym_hmm_ec.envs.bipedal_env import BipedEnv 
-# from gym_hmm_ec.controllers.pd_controller import PDController 
-# import matplotlib.pyplot as plt
-# import numpy as np
-# from mujoco_py import functions
+# keep the similation in pause until activated manually
+if env.env_params['render']:
+    env.viewer._paused = True
 
-# # environment config and setup
-# env_conf = {
-#             'set_on_rack': False,
-#             'render': True,
-#             'model_name':'humanoid_CMU_no_hands',
-#             'mocap':False
-#             }
-
-# env = BipedEnv(**env_conf)
-
-# # initialse the env,reset simualtion
-# env.reset()
-
-# # keep the similation in pause until activated manually
-# if env.env_params['render']:
-#     env.viewer._paused = True
-
-# for _ in range(2000):
+for _ in range(2000):
     
-#     obs,reward,done,info = env.step(action = np.zeros(shape=env.n_act_joints))
+    obs,reward,done,info = env.step(action = np.zeros(shape=env.n_act_joints))
 
 
-# env.close()
+env.close()
